3509f16632fadd8d721abc71ecd0fcf857f0589f
MJets09 <mthaoqk@gmail.com>
2022-09-25 18:40:34 -0500

    9.25

--- textGame/up-the-mountain-starter.lua
+++ textGame/up-the-mountain-starter.lua
@@ -1,31 +1,6 @@
---[[
-    Starter code for "Up the Mountain"
-    CSCI 1011 Text game assignment
-    Mithat Konar
---]]
+local stuff = {}
+stuff.theAnswer = 42
 
--- constants needed in the program
-local ENERBERRY_CALORIES = 10
-local ZAPBERRY_CALORIES = 12
-
--- variables needed in the program
-local rivotCalories = 42    --[[ the number of calories a rivot at the very
-                                 start. ]]
-local isEnerberry           --[[ a boolean used to store whether the rivot
-                                 ate an enerberry or a zapberry. ]]
-
--- Let's do some housekeeping.
-math.randomseed(os.time()) -- seed the random number generator
-
--- Let's begin!
--- The rivot climbs 100 meters, so it's time to eat.
--- Did the rivot eat an enerberry or zapberry (a 50/50 proposition)?
-isEnerberry =  math.random(0, 1) == 1  -- half the time this will be true.
-
-if isEnerberry then
-    rivotCalories = rivotCalories + ENERBERRY_CALORIES
-    print(rivotCalories)
-else
-    rivotCalories = rivotCalories - ZAPBERRY_CALORIES
-    print(rivotCalories)
+for i,v in pairs(stuff) do
+    print(i ..' : ' .. v)
 end
--- textGame/upTheMountain.lua
+++ textGame/upTheMountain.lua
@@ -1,27 +1,59 @@
+math.randomseed(os.time())
 local enerBerries = 10
 local zapBerries = 12
-local rivotCal = 42
+local rivotCal = math.random(34,50)
 local isEBerries
+local playerRivot
+local playerName
+local counter = 0
+local i = 0
 
 
-print(isEBerries)
 
+function greet()
+  io.write("Welcome to the Tua Peaks, where the greatest Rivots have trained. Greetings trainer, what is your name? ")
+  playerName = io.read()
+  io.write("Welcome, " .. playerName .. " which Rivot would take the honor of climbing the peaks? ")
+  playerRivot = io.read()
+  print("Well then, " .. playerRivot .. ". Let us begin your training, we will test " .. playerRivot .. "'s strengh.")
+  print("Every 100 meters " .. playerRivot .. " will have the chance to rest and recover their strengh. Let us see how relentess " .. playerRivot .. " is. ")
+  print(playerRivot .. " has ascended the first 100 meters, and base on their energy tracker, they are at " ..rivotCal .. " calories and now is looking for food.")
 
-while rivotCal > 20 do
-print("Climb")
+  end
+    
+    
+greet() 
 
-math.randomseed(os.time())
+ 
+while rivotCal > 25 do
+  
 isEBerries = math.random(0,1)
 
+
 if isEBerries == 0 then
-  --if this is not true, I would run this code
-rivotCal = rivotCal + isEBerries
---I'll add 12 to my current rivotCal at 42 which becomes 52
-print(rivotCal)
---Prints 52, so it would run the code again and add 12 again correct?
+  rivotCal = rivotCal + enerBerries
+  print(playerRivot .. " has ascended another 100 meters. " .. playerRivot .. " has acquired an enerberry, gaining some energy. They're currently sitting at " .. rivotCal .. " calories.")
+  counter = i + i
+  i = i + 100
 else
-rivotCal = rivotCal - zapBerries
-print(rivotCal)
+  rivotCal = rivotCal - zapBerries
+  print(playerRivot .. " has ascended another 100 meters. " .. playerRivot .. " has acquired an zapberry, they don't look so good. They're currently sitting at " .. rivotCal .. " calories.")
+  counter = i + i
+  i = i + 100
 end
 
+
+if rivotCal < 32 then
+  print(playerRivot .. " would need to stop soon, his calories are getting low.")
+end
+
+end
+
+
+if(counter > 1000) then
+print(playerRivot .. " has ascended " .. counter .. " meters. Mighty impression")
+elseif (counter < 500) then
+print(playerRivot .. " has ascended " .. counter .. " meters. Not too shabby.")
+elseif (counter < 300) then
+print(playerRivot .. " has ascended " .. counter .. " meters. Not to far, we can always try again.")
 end
